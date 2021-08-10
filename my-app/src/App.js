cc98a99790bde6ede97bca5042220dd07f3eb978
Tanay Dwivedi <tanaydwivedi456@gmail.com>
2021-08-10 12:33:04 +0530

    feature:added add in cart feature

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "react-toasify/dist/ReactToastify.css"
 import './App.css';
 
-function App() {
+const App=()=> {
+  const [cartItem,setCartItem]=useState([])
+  const addInCart=item=>{
+
+    const isAlreadyAdded=cartItem.findIndex(function(array){
+      return array.id===item.id;
+    });
+      if(isAlreadyAdded!==-1){
+        toast("already added in cart",{
+          type:"error"
+        });
+      }
+
+      setCartItem([...cartItem,item])
+
+    
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
