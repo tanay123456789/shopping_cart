fdba5d46237f5bc889385644e56bce9377e0f591
Tanay <tanaydwivedi456@gmail.com>
2021-09-02 18:08:57 +0530

    feature:added API

--- my-app/src/Components/BuyPage.js
+++ my-app/src/Components/BuyPage.js
@@ -5,7 +5,8 @@ import { random,commerce } from "faker";
 import {Container,Col,Row} from "reactstrap"
 
 
-const apiKey=""
+const apiKey=process.env.REACT_APP_API_KEY;
+
 
 const url="https://api.pexels.com/v1/search?query=clothes&per_page=6&page=1"
 
@@ -15,12 +16,17 @@ const url="https://api.pexels.com/v1/search?query=clothes&per_page=6&page=1"
 const BuyPage=({addInCart})=>{
 
     const [product,setProduct]=useState([])
-    const fetchPhotos=async=()=>{
+    const fetchPhotos=async()=>{
         const response=await Axios.get(url,{
             header:{
                 Authorization:apiKey
             }
         })
+        const { photos } = data;
+        useEffect(() => {
+            fetchPhotos();
+        }, []);
+};
     }
 
 }
