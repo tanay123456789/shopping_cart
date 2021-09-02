cb9f22417055409f805df956e8e59e0c95c3d119
Tanay <tanaydwivedi456@gmail.com>
2021-09-02 11:50:13 +0530

    fix:positioning of fetchPhotos

--- my-app/src/Components/BuyPage.js
+++ my-app/src/Components/BuyPage.js
@@ -9,18 +9,19 @@ const apiKey=""
 
 const url="https://api.pexels.com/v1/search?query=clothes&per_page=6&page=1"
 
-const fetchPhotos=async=()=>{
-    const response=await Axios.get(url,{
-        header:{
-            Authorization:apiKey
-        }
-    })
-}
+
 
 
 const BuyPage=({addInCart})=>{
 
     const [product,setProduct]=useState([])
+    const fetchPhotos=async=()=>{
+        const response=await Axios.get(url,{
+            header:{
+                Authorization:apiKey
+            }
+        })
+    }
 
 }
 
